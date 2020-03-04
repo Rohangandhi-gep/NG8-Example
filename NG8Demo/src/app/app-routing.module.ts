266ea92f180494c0ed9c221b3a26316d0fdc6f5a
Sai Pavan <sai.kumar1@gep.com>
2020-03-03 23:18:12 -0500

    folder structure setup

--- NG8Demo/src/app/app-routing.module.ts
+++ NG8Demo/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EntryComponent } from './entry/component/entry/entry.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'page1', pathMatch: 'full' },
+  { path: 'page1', component: EntryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ NG8Demo/src/app/plugins/textbox/textbox.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TextboxComponent } from './textbox/textbox.component';
+
+
+
+@NgModule({
+  declarations: [TextboxComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TextboxModule { }
