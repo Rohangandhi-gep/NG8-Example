85fca0f6266000614500d89d97af0eb8d547cbe1
makeshkurup-gep <makesh.kurup@gep.com>
2020-03-04 20:58:03 -0500

    Dynamically loading components

--- NG8Demo/src/app/demopage/component/demopage.component.ts
+++ NG8Demo/src/app/demopage/component/demopage.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ViewContainerRef,
+  ComponentFactoryResolver,
+  ComponentRef,
+  ComponentFactory,
+  OnInit
+} from '@angular/core';
+import { TextboxComponent } from 'src/app/components/textbox/component/textbox.component';
+import { DemoPageDirective } from '../demopage.directive';
+import { TableComponent } from 'src/app/components/table/component/table.component';
 
 @Component({
   selector: 'app-demopage',
@@ -7,8 +18,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemopageComponent implements OnInit {
 
-  constructor() { }
 
+  componentRef: any;
+  componentFactory: any;
+  viewContainerRef:any;
+  @ViewChild(DemoPageDirective, {static: true}) demoPageDirective: DemoPageDirective;
+
+  constructor(private resolver: ComponentFactoryResolver) { }
+
+  createDynamicComponent(componentName) {
+    console.log(componentName)
+    if(componentName==="TextBox")
+    {
+      this.componentFactory = this.resolver.resolveComponentFactory(TextboxComponent);
+    }
+    else if(componentName==="Table")
+    {
+      this.componentFactory = this.resolver.resolveComponentFactory(TableComponent);
+    }
+
+    this.viewContainerRef = this.demoPageDirective.viewContainerRef;
+    this.viewContainerRef.clear();
+
+    this.componentRef = this.viewContainerRef.createComponent(this.componentFactory);
+  }
+  destroyComponent() {
+      this.componentRef.destroy();
+  }
   ngOnInit(): void {
   }
 
